1c1b35b9538454fd5652cca183388b81c8883908
magic1979 <salvatore.bruni@gmail.com>
2015-07-16 20:07:00 +0200

    v.11.5
    v.11.5

--- www/js/Catalogo.js
+++ www/js/Catalogo.js
@@ -6,6 +6,17 @@ function onDeviceReady() {
 	window.addEventListener('load', function() {
 		FastClick.attach(document.body);
 	}, false);
+	
+	last_click_time = new Date().getTime(); 
+document.addEventListener('click', function (e) { 
+click_time = e['timeStamp']; 
+if (click_time && (click_time - last_click_time) < 1000) { e.stopImmediatePropagation(); 
+e.preventDefault(); 
+return false; 
+} 
+last_click_time = click_time; 
+}, true);
+
     
     $.mobile.defaultPageTransition = 'none';
     $.mobile.defaultDialogTransition = 'none';
